11e098ebe5318bb4a77b6a772fc4b1b617c4df13
gaspard1-sys <gnsengiyumva96@gmail.com>
2025-03-18 13:38:48 +0400

    version three

--- pyfum.py
+++ pyfum.py
@@ -4,13 +4,20 @@
 # word=input("type your word: ")
 # first_word=word.replace(" ", "")
 # print(f"your word has this number of charcter {len(first_word)}")
-m=5
-string=[]
-for i in range(1,6):
-   text = input(f"enter string{i}")
-   string.append(text)
-   i=i 
-for name in string:
-    word=name.replace(" ", "")
-    print(f"number of this charcter is {len(word)}")
+# m=5
+# string=[]
+# for i in range(1,6):
+#    text = input(f"enter string{i}")
+#    string.append(text)
+#    i=i 
+# for name in string:
+#     word=name.replace(" ", "")
+#     print(f"number of this charcter is {len(word)}")
 
+text=input("enter text: ")
+text=text.replace("a","k")
+text=text.replace("i","j")
+text=text.replace("o","p")
+text=text.replace("e","t")
+text=text.replace("o","f")
+print(text)
