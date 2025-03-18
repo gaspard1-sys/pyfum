2f48925f7e1c60e88398e0667b3f8fa2d09aa502
gaspard1-sys <gnsengiyumva96@gmail.com>
2025-03-18 13:29:39 +0400

    version three

--- pyfum.py
+++ pyfum.py
@@ -1,8 +1,16 @@
 # print("hello world")
 # name=input("enter your name here:  ")
 # print(f"your name has {len(name)} charcters")
-word=input("type your word: ")
-first_word=word.replace(" ", "")
-print(f"your word has this number of charcter {len(first_word)}")
+# word=input("type your word: ")
+# first_word=word.replace(" ", "")
+# print(f"your word has this number of charcter {len(first_word)}")
+m=5
+string=[]
+for i in range(1,6):
+   text = input(f"enter string{i}")
+   string.append(text)
+   i=i 
+for name in string:
+    word=name.replace(" ", "")
+    print(f"number of this charcter is {len(word)}")
 
-      
