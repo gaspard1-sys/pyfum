58ad96d275409528dd23159864f58b8e1bc19682
gaspard1-sys <gnsengiyumva96@gmail.com>
2025-03-18 11:11:54 +0400

    version two

--- pyfum.py
+++ pyfum.py
@@ -1,3 +1,8 @@
-print("hello world")
-name=input("enter your name here:  ")
-print(f"your name has {len(name)} charcters")
+# print("hello world")
+# name=input("enter your name here:  ")
+# print(f"your name has {len(name)} charcters")
+word=input("type your word: ")
+first_word=word.replace(" ", "")
+print(f"your word has this number of charcter {len(first_word)}")
+
+      
